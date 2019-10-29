d5d49baaaf936553d6d67b45afe99e0836759600
Ronald Theodoro <ronaldtheodorodigesto@gmail.com>
2019-10-29 10:46:19 -0300

    Add app core

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,16 @@
-import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
+import { NgModule } from "@angular/core";
+import { Routes, RouterModule } from "@angular/router";
+import { CoreComponent } from "./core/core.component";
 
-
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    component: CoreComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
