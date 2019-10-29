8ba88b5fcc1829d16a3140be3565ef46c49b0954
Ronald Theodoro <ronaldtheodorodigesto@gmail.com>
2019-10-29 14:45:21 -0300

    Show data

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,10 +1,15 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+import { HttpClientModule } from '@angular/common/http'; 
+
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { CoreComponent } from './core/core.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+import { GithubApiService } from './services/github-api.service';
+import { MatTableModule, MatPaginatorModule, MatSortModule } from '@angular/material';
 
 @NgModule({
   declarations: [
@@ -14,9 +19,13 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    BrowserAnimationsModule
+    MatTableModule,
+    MatSortModule,
+    MatPaginatorModule,
+    HttpClientModule,
+    BrowserAnimationsModule,
   ],
-  providers: [],
+  providers: [GithubApiService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
--- src/app/core/core.component.ts
+++ src/app/core/core.component.ts
@@ -1,15 +1,59 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ViewChild, AfterViewInit } from "@angular/core";
+import { HttpClient } from "@angular/common/http";
+import { MatPaginator } from "@angular/material/paginator";
+import { MatSort } from "@angular/material/sort";
+import { merge, of as observableOf } from "rxjs";
+import { catchError, map, startWith, switchMap } from "rxjs/operators";
+import { GithubApiService } from '../services/github-api.service';
+import { GithubIssue } from '../models/github-issue.model'
 
 @Component({
-  selector: 'app-core',
-  templateUrl: './core.component.html',
-  styleUrls: ['./core.component.css']
+  selector: "app-core",
+  templateUrl: "./core.component.html",
+  styleUrls: ["./core.component.css"]
 })
-export class CoreComponent implements OnInit {
+export class CoreComponent implements AfterViewInit {
+  displayedColumns: string[] = ['created', 'state', 'number', 'title'];
+  githubApi: GithubApiService | null;
+  data: GithubIssue[] = [];
 
-  constructor() { }
+  resultsLength = 0;
+  isLoadingResults = true;
+  isRateLimitReached = false;
 
-  ngOnInit() {
+  @ViewChild(MatPaginator, {static: false}) paginator: MatPaginator;
+  @ViewChild(MatSort, {static: false}) sort: MatSort;
+
+  constructor(private _http: HttpClient) {}
+
+  ngAfterViewInit() {
+    this.githubApi = new GithubApiService(this._http);
+
+    this.sort.sortChange.subscribe(() => this.paginator.pageIndex = 0);
+
+    merge(this.sort.sortChange, this.paginator.page).pipe(
+      startWith({}),
+      switchMap(() => {
+        this.isLoadingResults = true;
+        return this.githubApi!.getRepoIssues(
+          this.sort.active,
+          this.sort.direction,
+          this.paginator.pageIndex,
+        )
+      }),
+      map(data => {
+        this.isLoadingResults = false;
+        this.isRateLimitReached = false;
+        this.resultsLength = data.total_count;
+
+        return data.items;
+      }),
+      catchError(() => {
+        this.isLoadingResults = false;
+        this.isRateLimitReached = true;
+        return observableOf([]);
+      })
+    ).subscribe(data => this.data = data);
   }
 
 }
